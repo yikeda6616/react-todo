b338a46e9833ab7b16fc491f80a8fa36f717ee44
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-06 16:16:39 -0700

    add sass setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.scss';
 
 class App extends React.Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 // Constants
 const MODE = 'development';
+const enabledSourceMap = MODE === 'development';
 
 module.exports = {
   mode: MODE,
 
   entry: './src/index.js',
+
+  devtool: 'source-map',
 
   module: {
     rules: [
@@ -18,6 +21,48 @@ module.exports = {
             }
           }
         ]
+      },
+
+      {
+        test: /\.scss/,
+        use: [
+          // style-loader will embed <link> tag into <head> tag
+          'style-loader',
+
+          // css-loader will bundle .css into js
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              sourceMap: enabledSourceMap,
+              /*
+               * 0 => no loaders (default)
+               * 1 => postcss-loader
+               * 2 => postcss-loader, sass-loader
+               */
+              importLoaders: 2
+            }
+          },
+          // postcss-loader will enable autoprefixer
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+              plugins: [
+                // Enable Autoprefixer
+                require('autoprefixer')({ grid: true })
+              ]
+            }
+          },
+
+          // sass-loader will compile .scss into .css
+          { loader: 'sass-loader', options: { sourceMap: enabledSourceMap } }
+        ]
+      },
+      {
+        // Target file extensions
+        test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+        loader: 'url-loader'
       }
     ]
   },
